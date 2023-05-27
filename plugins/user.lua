5ace1e9106bd6a4e4b69e80883cda5985005dd33
Parag Meshram <parag.meshram@hotmail.com>
2023-05-27 15:34:24 +0530

    Added themes

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,8 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
+  { "catppuccin/nvim", name = "catppuccin" },
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
